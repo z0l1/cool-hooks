bdb4cbd2d4772caedb3ff2fb7dcd489e4e342305
z0l1 <44405911+z0l1@users.noreply.github.com>
2021-09-14 11:18:22 +0200

    Create useMediaQuery.ts
    simple useMediaQuery hook

---
+++ useMediaQuery.ts
@@ -0,0 +1,31 @@
+// inspired by https://usehooks-typescript.com/react-hook/use-dark-mode
+import { useCallback, useEffect, useState } from "react";
+
+const useMediaQuery = (query: string): boolean => {
+	const doesMatch = useCallback(
+		(query: string): boolean => {
+			if (!window) return false;
+			return window.matchMedia(query).matches;
+		},
+		[query]
+	);
+
+	const [matches, setMatches] = useState<boolean>(doesMatch(query));
+
+	useEffect(() => {
+		const handler = () => setMatches(doesMatch(query));
+		const matchMedia = window.matchMedia(query);
+
+		matchMedia.addEventListener("change", handler);
+
+		return () => {
+			matchMedia.removeEventListener("change", handler);
+		};
+	}, []);
+
+	useEffect(() => setMatches(doesMatch(query)), [query, doesMatch]);
+
+	return matches;
+};
+
+export default useMediaQuery;
