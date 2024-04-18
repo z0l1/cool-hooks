fce24e7641e6e10c90043360cf1d88bf6fa2e8e6
z0l1 <44405911+z0l1@users.noreply.github.com>
2024-04-18 08:51:19 +0200

    Create useStateManager.ts

---
+++ useStateManager.ts
@@ -0,0 +1,50 @@
+import { ChangeEvent } from "react";
+
+export function useStateManager<T>(
+	data: T,
+	setData: (data: T) => void,
+	options: {
+		setValueValidator?: (key: keyof T, value: any) => boolean;
+		onSetValue?: (key: keyof T, value: any) => void;
+		onSet?: () => void;
+	}
+): {
+	setValue: (key: keyof T, value: any) => void;
+	inputValueSetter: (key: keyof T, afterFunc?: () => void) => void;
+	inputNumberValueSetter: (key: keyof T, afterFunc?: () => void) => void;
+} {
+	const setValue = (key: keyof T, value: any) => {
+		if (options.setValueValidator !== undefined) {
+			if (options.setValueValidator?.(key, value) === true) {
+				return;
+			}
+		}
+
+		setData?.({ ...data, [key]: value });
+		options.onSetValue?.(key, value);
+		options.onSet?.();
+	};
+
+	const inputValueSetter = <TI = HTMLInputElement>(key: keyof T, afterFunc?: () => void) => {
+		return (e: ChangeEvent<TI>) => {
+			setValue(key, e.target.value);
+			afterFunc?.();
+		};
+	};
+
+	const inputNumberValueSetter = <TI = HTMLInputElement>(
+		key: keyof T,
+		afterFunc?: () => void
+	) => {
+		return (e: ChangeEvent<TI>) => {
+			setValue(key, Number(e.target.value));
+			afterFunc?.();
+		};
+	};
+
+	return {
+		setValue,
+		inputValueSetter,
+		inputNumberValueSetter,
+	};
+}
